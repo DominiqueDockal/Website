39dc49f4c8e7bb78a70209fdb9cdd6d3cc1af6f5
DominiqueDockal <dominique.dockal@gmail.com>
2025-06-18 11:53:55 +0200

    routing implemented legalNotice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { LegalNoticeComponent } from './components/legal-notice/legal-notice.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainContentComponent,
+    title: 'Startpage'
+  },
+  {
+    path: 'legalNotice',
+    component: LegalNoticeComponent,
+    title: 'LegalNotice'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
