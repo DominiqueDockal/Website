9ba1f0b830899bf01faf7766b119c245736c9df8
DominiqueDockal <dominique.dockal@gmail.com>
2025-06-21 18:47:37 +0200

    about me first implementation

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
