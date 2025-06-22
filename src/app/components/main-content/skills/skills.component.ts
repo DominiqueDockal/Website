761a77cc511686356e070dd75316c6163a6ea949
DominiqueDockal <dominique.dockal@gmail.com>
2025-06-22 12:19:27 +0200

    skills implemented

--- src/app/components/main-content/skills/skills.component.ts
+++ src/app/components/main-content/skills/skills.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  onButtonClick(event: Event): void {
+    const target = event.target as HTMLElement;
+    target.style.animation = 'none';
+    
+    // Hier  Navigation/Weiterleitung
+    // z.B. this.router.navigate(['/contact']);
+    
+    console.log('Button clicked - navigation logic here');
+}
+  
 
 }
--- src/app/services/translations.ts
+++ src/app/services/translations.ts
@@ -10,10 +10,17 @@ export const TRANSLATIONS: { [K in 'de' | 'en']: Record<string, string> } = {
 
     'about.box-title' : 'Wer Bin Ich',
     'about.box-headline': 'Über mich',
-    'about.main-text': 'Hey, ich bin Dominique. Hier kommt bald ein Text',
-    'about.location': 'Ich komme aus Stuttgart',
-    'about.mindset': 'Das ist mein Mindset',
-    'about.problem-solving': 'So löse ich Probleme',
+    'about.main-text': 'Hey, ich bin Dominique. Mein Bachelor in Informatik \
+                        verleiht meiner praktischen Programmiererfahrung eine solide \
+                        theoretische Fundierung. Diese Kombination ermöglicht es mir, \
+                        nachhaltige und durchdachte Lösungen zu finden',
+    'about.location': 'Stuttgart oder Remote - flexibel und fokussiert.',
+    'about.mindset': 'Ich bin von Natur aus neugierig und lerne neue Technologien schnell. \
+                      Jede Herausforderung sehe ich als Chance, meine Fähigkeiten zu erweitern \
+                      und innovative Lösungsansätze zu entwerfen.',
+    'about.problem-solving': 'Beim Problemlösen gehe ich strukturiert und analytisch vor. \
+                              Ich zerlege komplexe Aufgaben systematisch, erkenne Muster und \
+                              Zusammenhänge schnell und entwickle Lösungen, die sowohl elegant als auch präzise sind.',
 
   },
   en: {
@@ -27,10 +34,15 @@ export const TRANSLATIONS: { [K in 'de' | 'en']: Record<string, string> } = {
 
     'about.box-title' : 'Who I Am',
     'about.box-headline': 'About me',
-    'about.main-text': 'Hey, I\'m Dominique, there will be soon some text',
-    'about.location': 'I\'m based in Stuttgart',
-    'about.mindset': 'That\'s my mindset',
-    'about.problem-solving': 'That\'s how I solve problems',
+    'about.main-text': 'Hey, I\'m Dominique. My Bachelor\'s degree in Computer Science provides a solid theoretical \
+                        foundation to complement my practical programming experience. This combination enables me \
+                        to develop sustainable and well-thought-out solutions.',
+    'about.location': 'Stuttgart or remote - flexible and focused',
+    'about.mindset': 'I am naturally curious and quickly learn new technologies. I see every challenge as an opportunity \
+                      to expand my skills and devise innovative solutions.',
+    'about.problem-solving': 'When solving problems, I take a structured and analytical approach. \
+                              I systematically break down complex tasks, quickly recognize patterns \
+                              and connections, and develop solutions that are both elegant and precise.',
 
   }
 };
